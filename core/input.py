570cd02ba56a2872c849f97ff19db59de1dd9ab5
akshnova <akshayb@gmx.com>
2023-06-22 10:14:20 +0530

    scrappped temp and test files

--- core/input.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-from .core import core
-
-def lockInput( window : Tk):
-    core.win = window
-    core.win.bind('<Escape>', core.unlockInput)
-    core.win.bind('<KeyPress>', core.keyInput)
-    core.win.bind('<Motion>', core.mouseInput)
-    core.win.config(cursor="none")
